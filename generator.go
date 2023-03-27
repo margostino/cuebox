1fb9ac6908168b8a5b538ff1575054af936c87d7
Martin D'Agostino <martin.dagostino@klarna.com>
2023-03-27 14:43:48 +0200

    comment

--- generator.go
+++ generator.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"cuelang.org/go/cue"
-	"cuelang.org/go/encoding/gocode"
-	"io/ioutil"
 )
 
 const config = `
@@ -14,16 +12,16 @@ place: string | *"world" // "world" is the default.
 var r cue.Runtime
 
 func main() {
-	instance, _ := r.Compile("test", config)
-
-	str, _ := instance.Lookup("msg").String()
-	println(str)
-
-	b, err := gocode.Generate("github.com/margostino/cuebox", instance, nil)
-	if err != nil {
-		// handle error
-		println(err.Error())
-	}
-
-	err = ioutil.WriteFile("cue_gen.go", b, 0644)
+	//instance, _ := r.Compile("test", config)
+	//
+	//str, _ := instance.Lookup("msg").String()
+	//println(str)
+	//
+	//b, err := gocode.Generate("github.com/margostino/cuebox", instance, nil)
+	//if err != nil {
+	//	// handle error
+	//	println(err.Error())
+	//}
+	//
+	//err = ioutil.WriteFile("cue_gen.go", b, 0644)
 }
